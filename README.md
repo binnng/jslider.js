a7a3e1ee1d6af036201a071df58440e2aa9e7787
binnng <imbinnng@gmail.com>
2013-03-21 18:03:59 +0800

    继续完善，添加类型

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-slider
-======
+## 描述
+**jslider.js** 是一个基于jQuery的图片轮播器插件。
 
+### 使用示例
+
+```javascript
+JSlider({
+	id: 'slider',
+	type: 'fade', 
+	interval: 1000
+});
+```
+
+### 参数
+* **id**: `String` 容器的id，可以不指定，默认为slider
+* **type**: `String` 轮播器切换效果，有`none`不切换，`slide`普通滑动，`infinite`无限滑动和`fade`淡入淡出四种类型。
+* **interval**: `Number` 轮播器切换的间隔时间。
--- jslider.js
+++ jslider.js
@@ -8,12 +8,11 @@
 
 	var jslider = function(conf) {
 
-		this.id   = conf.id   || 'slider';
-		this.type = conf.type || 'fade';
+		this.id       = conf.id       || 'slider';
+		this.type     = conf.type     || 'infinite';
+		this.interval = conf.interval || 5000;
 
-		this.autoplay = conf.init || true;
-
-		this.autoplay && this.init();
+		this.init();
 
 		return this;
 
@@ -21,28 +20,62 @@
 
 	jslider.prototype.init = function() {
 
-		var slider = $('#' + this.id).css('overflow', 'hidden'),
+		var slider      = $('#' + this.id).css('overflow', 'hidden'),
+			width       = slider.width(),
 			sliderInner = (slider.html( '<div id="slider_inner">' + slider.html() + '</div>' ), $('#slider_inner')),
 
-			imgs = sliderInner.children('img'),
-			size = imgs.length,
-			curImg = 0,
-			nexImg;
+			imgs        = sliderInner.children('img'),
+			size        = imgs.length,
+			curImg      = 0,
+			nexImg,
+
+			fn;
 
 		sliderInner.css({height:'100%'});
 
+		if ( this.type === 'fade' ) {
+			imgs.css({position: 'absolute', display: 'none'});
+			imgs.eq(0).css({zIndex: 2, display: 'block'});
+		} else {
+			sliderInner.css('width', width * size);
+			imgs.css({display: 'block', float: 'left'});
+		}
+
+		switch ( this.type ) {
+			case 'none' : fn = function() {
+				sliderInner.css({marginLeft:- width * nexImg});
+			};
+			break;
+			case 'fade' : fn = function() {
+
+				imgs.eq(nexImg).css('z-index', 2).fadeIn();
+				imgs.eq(curImg).css('z-index', 1).fadeOut();
+				
+			};
+			break;
+			case 'slide' : fn = function() {
+
+				sliderInner.animate({marginLeft:- width * nexImg});
+
+			};
+			break;
+			case 'infinite' : fn = function() {
+fn
+				sliderInner.animate({marginLeft:- width}, function() {
+					imgs.eq(curImg - 1).appendTo(sliderInner.css('margin-left', 0));
+				});
+
+
+			}
+		}
+
 		this.timer = setInterval(function() {
 
-			nexImg = nexImg === size - 1 ? 1:curImg + 1;
-
-			console.log(curImg + ':' + nexImg);
-
-			imgs.eq(curImg).css('z-index', 2).fadeOut();
-			imgs.eq(nexImg).css('z-index', 1).fadeIn();
-
+			nexImg = nexImg === size - 1 ? 0 : curImg + 1;
+			fn();
 			curImg = nexImg;
-			
-		}, 1000);
+
+		}, this.interval);
 		
 	};
 
